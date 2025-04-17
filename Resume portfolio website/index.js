8c0ecb32ebe6282c15c8a14a7ada737a941f7ca1
Nishu Kumari <123900586+Nishu1044@users.noreply.github.com>
2025-04-17 23:29:00 +0530

    Update index.js

--- Resume portfolio website/index.js
+++ Resume portfolio website/index.js
@@ -19,7 +19,18 @@ loop : true
 // end //
 
 
-
+function openAndDownload() {
+    // Open Google Drive preview in new tab
+    window.open("https://drive.google.com/file/d/1VBRqbRHYs3xIblbtaDOzNTizxBoOXv_m/view", "_blank");
+    
+    // Trigger file download
+    const a = document.createElement("a");
+    a.href = "https://drive.google.com/uc?export=download&id=1VBRqbRHYs3xIblbtaDOzNTizxBoOXv_m";
+    a.download = "Nishu_Resume.pdf"; // Optional: rename the downloaded file
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
 
 
 
